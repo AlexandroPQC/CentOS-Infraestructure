abab582f1c17196257c1a15bc1c8e93fc494430a
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2016-06-30 17:03:41 -0400

    Enabling eth2

--- firewall.sh
+++ firewall.sh
@@ -3,12 +3,18 @@ source functions.sh
 
 # Ennabling interfaces
 
-
 enable_interfaces(){
 	echo "Habilitando interfaces..."
-	config_file scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0 
-	config_file scripts/ifcfg-eth1 /etc/sysconfig/network-scripts/ifcfg-eth1
+	cp /etc/sysconfig/network-scripts/ifcfg-eth2 scripts/ifcfg-eth2
+	sed 's/ONBOOT=no/ONBOOT=yes/g' ./scripts/ifcfg-eth2
 	config_file scripts/ifcfg-eth2 /etc/sysconfig/network-scripts/ifcfg-eth2
+
+	cp /etc/sysconfig/network-scripts/ifcfg-eth0 scripts/ifcfg-eth0
+	config_file scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0
+
+	cp /etc/sysconfig/network-scripts/ifcfg-eth1 scripts/ifcfg-eth1
+	config_file scripts/ifcfg-eth1 /etc/sysconfig/network-scripts/ifcfg-eth1
+
 }
 
 bonding(){
@@ -16,13 +22,14 @@ bonding(){
 	echo "alias	bond0	bonding" > /etc/modprobe.d/bonding.conf
 	echo "options	bonding	mode=0	miimon=30" >> /etc/modprobe.d/bonding.conf
 	depmod -a
-	
-}	
 
-if [ -e "/sys/class/net/eth0" -a -e "/sys/class/net/eth1" -a -e "/sys/class/net/eth2" ]
-then
+}
+
+# Requirements
+	if [ -e "/sys/class/net/eth0" -a -e "/sys/class/net/eth1" -a -e "/sys/class/net/eth2" ]
+	then
 	enable_interfaces
-	bonding
+	#bonding
 else
 	echo "No se encontraron todas las interfaces de red"
 fi
